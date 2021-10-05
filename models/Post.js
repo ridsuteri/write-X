3281bd3203a17cb68b382d85cc5ff53ba717a504
Riddhi Suteri <ridsuteri@gmail.com>
2021-10-05 15:42:53 +0530

    add author name and avatar in published post

--- models/Post.js
+++ models/Post.js
@@ -1,5 +1,6 @@
-const postsCollecetion = require("../db").db().collection("posts");
+const postsCollection = require("../db").db().collection("posts");
 const ObjectID = require("mongodb").ObjectId;
+const User = require("./User");
 
 let Post = function (data, userid) {
   this.data = data;
@@ -42,7 +43,7 @@ Post.prototype.create = function () {
     this.validate();
 
     if (!this.errors.length) {
-      postsCollecetion
+      postsCollection
         .insertOne(this.data)
         .then(() => {
           resolve();
@@ -63,10 +64,41 @@ Post.findSingleById = function (id) {
       reject();
       return;
     }
+    let posts = await postsCollection
+      .aggregate([
+        { $match: { _id: new ObjectID(id) } },
+        {
+          $lookup: {
+            from: "users",
+            localField: "author",
+            foreignField: "_id",
+            as: "authorDocument",
+          },
+        },
+        {
+          $project: {
+            title: 1,
+            body: 1,
+            createdDate: 1,
+            author: { $arrayElemAt: ["$authorDocument", 0] },
+          },
+        },
+      ])
+      .toArray();
 
-    let post = await postsCollecetion.findOne({ _id: ObjectID(id) });
-    if (post) {
-      resolve(post);
+    // clean up author property in each post object
+    posts = posts.map(function (post) {
+      post.author = {
+        username: post.author.username,
+        avatar: new User(post.author, true).avatar,
+      };
+
+      return post;
+    });
+
+    if (posts.length) {
+      console.log(posts[0]);
+      resolve(posts[0]);
     } else {
       reject();
     }
--- models/User.js
+++ models/User.js
@@ -3,9 +3,15 @@ const bcrypt = require("bcryptjs");
 const validator = require("validator");
 const md5 = require("md5");
 
-let User = function (data) {
+let User = function (data, getAvatar) {
   this.data = data;
   this.errors = [];
+  if (getAvatar == undefined) {
+    getAvatar = false;
+  }
+  if (getAvatar) {
+    this.getAvatar();
+  }
 };
 
 User.prototype.cleanUp = function () {
